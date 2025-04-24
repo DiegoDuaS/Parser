6f81d25e9c17d5135cda88a268532dfc1ef61278
Fabiola Contreras <fcontrerascol@gmail.com>
2025-04-24 17:16:39 -0600

    fix: completely clean follow map

--- demo/src/main/java/modules/tables/pos/followpos.java
+++ demo/src/main/java/modules/tables/pos/followpos.java
@@ -11,13 +11,12 @@ import models.Grammar;
 
 public class followpos {
     private Grammar grammar;
-    private Map<String, Set<String>> firstMap = new HashMap<>();
+    private Map<String, Set<String>> firstMap;
     private Map<String, Set<String>> followMap = new HashMap<>();
 
     public followpos(Grammar grammar, Map<String, Set<String>> firstMap) {
         this.grammar = grammar;
         this.firstMap = firstMap;
-        followMap = new HashMap<>();
 
         // Incializar sets vacios en el mapa follow
         for (String no_terminal_cabeza : grammar.getProductions().keySet()) {
@@ -54,19 +53,31 @@ public class followpos {
             }
         }
 
-        // Buscar centinelas, ver si un follow depende de otro
-        for (String no_terminal_cabeza : followMap.keySet()) {
-            Set<String> thisFollowSet = followMap.get(no_terminal_cabeza); // Follow actual
-            Set<String> newFollowSet = new HashSet<>(); // Follow a modificar
-            for (String str_follow_element : thisFollowSet) {
-                if (grammar.getNoTerminales().contains(str_follow_element)) { // Si elemento es un no terminal
-                    // Copiar elementos 'follow' de otro key al key actual
-                    newFollowSet.addAll(followMap.get(str_follow_element));
-                } else {
-                    newFollowSet.add(str_follow_element); // Copiar los elementos terminales
+        int clean_sets = 0;
+        int totalNoTerminales = followMap.keySet().size();
+
+        while (clean_sets != totalNoTerminales) {
+            clean_sets = 0;
+            // Buscar centinelas, ver si un follow depende de otro
+            for (String no_terminal_cabeza : followMap.keySet()) {
+                Set<String> thisFollowSet = followMap.get(no_terminal_cabeza); // Follow actual
+                Set<String> newFollowSet = new HashSet<>(); // Follow a modificar
+
+                for (String str_follow_element : thisFollowSet) {
+                    if (grammar.getNoTerminales().contains(str_follow_element)) { // Si elemento es un no terminal
+                        // Copiar elementos 'follow' de otro key al key actual
+                        newFollowSet.addAll(followMap.get(str_follow_element));
+                    } else if (str_follow_element != null) {
+                        newFollowSet.add(str_follow_element); // Copiar los elementos terminales
+                    } // Omitir los nulos
+                }
+                followMap.put(no_terminal_cabeza, newFollowSet); // Reemplazar el conjunto actual con el nuevo conjunto
+
+                if (!grammar.getNoTerminales().stream().anyMatch(newFollowSet::contains)) {
+                    clean_sets++;
                 }
             }
-            followMap.put(no_terminal_cabeza, newFollowSet); // Reemplazar el conjunto actual con el nuevo conjunto
+
         }
 
         return followMap;
@@ -83,7 +94,7 @@ public class followpos {
      */
     private Set<String> check_rules(String B, int A_position, String[] produccion) {
         Set<String> result = new HashSet<>();
-        if (produccion[A_position].equals(B)) {
+        if (produccion[A_position].equals(B)) { // A = B
             return result;
         }
 
@@ -103,11 +114,11 @@ public class followpos {
             // considera que epsilon es guardado como un elemento nulo
             if (grammar.getProductions().get(first_beta).contains(null)) {
                 result.add(B); // B como centinela, debe usarse follow(B) al terminar
-                return result;
             }
 
             // aplico regla 2
-            return firstMap.get(first_beta);
+            result.addAll(firstMap.get(first_beta));
+            return result;
         }
 
         // aplico regla 2
@@ -119,22 +130,26 @@ public class followpos {
     public static void main(String[] args) {
         Grammar grammar = new Grammar();
 
-        grammar.agregarNoTerminal("S");
-        grammar.agregarNoTerminal("P");
-        grammar.agregarNoTerminal("Q");
+        grammar.agregarNoTerminal("E");
+        grammar.agregarNoTerminal("E'");
+        grammar.agregarNoTerminal("T");
+        grammar.agregarNoTerminal("T'");
+        grammar.agregarNoTerminal("F");
 
-        grammar.agregarTerminal("^");
-        grammar.agregarTerminal("V");
-        grammar.agregarTerminal("[");
-        grammar.agregarTerminal("]");
-        grammar.agregarTerminal("sentence");
+        grammar.agregarTerminal("+");
+        grammar.agregarTerminal("(");
+        grammar.agregarTerminal(")");
+        grammar.agregarTerminal("*");
+        grammar.agregarTerminal("id");
 
-        grammar.agregarProduccion("S", "S ^ P");
-        grammar.agregarProduccion("S", "P");
-        grammar.agregarProduccion("P", "P V Q");
-        grammar.agregarProduccion("P", "Q");
-        grammar.agregarProduccion("Q", "[ S ]");
-        grammar.agregarProduccion("Q", "sentence");
+        grammar.agregarProduccion("E", "T E'");
+        grammar.agregarProduccion("E'", "+ T E'");
+        grammar.agregarProduccion("E'", null);
+        grammar.agregarProduccion("T", "F T'");
+        grammar.agregarProduccion("T'", "* F T'");
+        grammar.agregarProduccion("T'", null);
+        grammar.agregarProduccion("F", "( E )");
+        grammar.agregarProduccion("F", "id");
 
         firstpos calc = new firstpos(grammar);
         Map<String, Set<String>> tablaFirst = calc.calcularFirstPos();
@@ -144,7 +159,7 @@ public class followpos {
         }
 
         followpos follow_calc = new followpos(grammar, tablaFirst);
-        Map<String, Set<String>> tablaFollow = follow_calc.getFollowPos("S");
+        Map<String, Set<String>> tablaFollow = follow_calc.getFollowPos("E");
         System.out.println("\nResultado FOLLOW:");
         for (Map.Entry<String, Set<String>> entry : tablaFollow.entrySet()) {
             System.out.println("Follow(" + entry.getKey() + ") = " + entry.getValue());
