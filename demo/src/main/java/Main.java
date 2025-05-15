77d9993125d94cd4da073de6f629a864354dd478
Fabiola Contreras <fcontrerascol@gmail.com>
2025-05-15 13:46:54 -0600

    feat: integrate process in main

--- demo/src/main/java/Main.java
+++ demo/src/main/java/Main.java
@@ -1,13 +1,55 @@
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import com.example.Modules.Analisis.Complete_Lex;
 import com.example.models.Token;
 
+import models.Grammar;
+import modules.input.yalpInterpreter;
+
 public class Main {
     public static void main(String[] args) {
         try {
-            List<Token> tokensLex = Complete_Lex.completeLex(""); // AÑADIR PARÁMETRO DE RUTA AL ARCHIVO DE CÓDIGO
+            // Ejecutar análisis léxico
+            // AÑADIR PARÁMETRO DE RUTA AL ARCHIVO DE CÓDIGO
+            // List<Token> Lex_tokens = Complete_Lex.completeLex("");
+
+            // Leer archivo yalp
+            yalpInterpreter file_reader = new yalpInterpreter();
+            file_reader.readFile("ejemplo.yalp");
+
+            Map<String, List<String>> productions = file_reader.getSavedProductions();
+            System.out.println(productions);
+            System.out.println();
+            // Comparar tokens recibidos con los obtenidos del generador léxico
+            Set<String> terminalesSet = new HashSet<>(file_reader.getSavedTokens());
+            System.out.println(terminalesSet);
+            // Set<String> tipos_tokens = new HashSet<>();
+            // for (Token token : Lex_tokens) {
+            // tipos_tokens.add(token.getTipo());
+            // }
+
+            // if (terminalesSet != tipos_tokens)
+            // System.err.println(
+            // "Hay discrepancias entre los tokens obtenidos por el analizador léxico y los
+            // tokens definidos en el archivo yalp");
+
+            terminalesSet.removeAll(file_reader.getIgnoredTokens());
+            List<String> terminales = new ArrayList<>(terminalesSet);
+
+            List<String> noTerminales = new ArrayList<>(productions.keySet());
+            System.out.println(noTerminales);
+            String initialSimbol = "";
+            if (noTerminales != null) {
+                initialSimbol = noTerminales.get(0);
+                System.out.println(initialSimbol);
+            }
+
+            Grammar grammar = new Grammar(productions, terminales, noTerminales, initialSimbol);
         } catch (IOException e) {
             e.printStackTrace();
         }
--- demo/src/main/java/models/Grammar.java
+++ demo/src/main/java/models/Grammar.java
@@ -11,8 +11,16 @@ public class Grammar {
     private List<String> noTerminales; // No terminales
     private String initialSimbol;
 
+    public Grammar(Map<String, List<String>> productions, List<String> terminales, List<String> noTerminales,
+            String initialSimbol) {
+        this.productions = productions;
+        this.terminales = terminales;
+        this.noTerminales = noTerminales;
+        this.initialSimbol = initialSimbol;
+    }
+
     public Grammar(String initialSimbol) {
-        this.productions =  new LinkedHashMap<>();
+        this.productions = new LinkedHashMap<>();
         this.terminales = new ArrayList<>();
         this.noTerminales = new ArrayList<>();
         this.initialSimbol = initialSimbol;
